c5a874c1225e065a807eb94e4eacf69aa0aa356d
Simon Gibbons <simongibbons@gmail.com>
2015-03-31 19:43:36 +0100

    Added author information

--- gvSelect/__init__.py
+++ gvSelect/__init__.py
@@ -1,3 +1,6 @@
 __all__ = ['GVSelectFile', 'GVSelection']
+__author__ = "Simon Gibbons (sljg2@ast.cam.ac.uk)"
+__version__ = "1.0"
+__copyright__ = "Copyright 2015 Simon Gibbons"
 
 from .gvSelect import GVSelectFile, GVSelection
